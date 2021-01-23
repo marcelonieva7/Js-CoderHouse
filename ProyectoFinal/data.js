934d92189f54be8d81a565bbf0d49fb4aa68d60d
marcelonieva7 <marcelonieva7@gmail.com>
2021-01-23 02:26:46 -0300

    segunda entrega

---
+++ ProyectoFinal/data.js
@@ -0,0 +1,28 @@
+const datos = 
+    {
+        "Cripto": [{
+            "especie": "Bitcoin",
+            "unidades": 10,
+            "precioIngreso" : 100,
+            "precioActual" : 130
+        },
+        {
+            "especie": "Ethereum",
+            "unidades": 11,
+            "precioIngreso" : 100, 
+            "precioActual" : 80
+        }],
+        "Acciones": [{
+            "especie": "Apple",
+            "unidades": 10,
+            "precioIngreso" : 100,
+            "precioActual" : 140
+        },
+        {
+            "especie": "Amd",
+            "unidades": 10,
+            "precioIngreso" : 100, 
+            "precioActual" : 110
+        }],
+        
+    }
